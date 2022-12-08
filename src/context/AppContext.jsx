371f0190d89d9427cef766f0cea5305e4ff444fc
Joao <joaoviniciusvitral@hotmail.com>
2022-12-08 19:29:05 -0300

    feat: updates

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -28,98 +28,89 @@ export function AppProvider({ children }) {
     setExpression("");
   };
 
-  const checkError = (exp) => {};
+  // const checkValueType = (exp) => {
+  //   // O retorno desta função é o tipo do valor da variavel
+  //   // Se for string, tem que ter aspas simples, boolean, true ou false, number, só numeros
 
-  const checkValueType = (exp) => {
-    // O retorno desta função é o tipo do valor da variavel
-    // Se for string, tem que ter aspas simples, boolean, true ou false, number, só numeros
+  //   // Aspas simples englobando o valor
+  //   if (exp.includes("'")) {
+  //     alert("string");
+  //     return "string";
+  //   } else if (exp.includes("true") || exp.includes("false")) {
+  //     // Se tiver aspas simples é string
+  //     if (exp.includes("'")) {
+  //       alert("string");
+  //       return "string";
+  //     }
 
-    // Aspas simples englobando o valor
-    if (exp.includes("'")) {
-      alert("string");
-      return "string";
-    } else if (exp.includes("true") || exp.includes("false")) {
-      // Se tiver aspas simples é string
-      if (exp.includes("'")) {
-        alert("string");
-        return "string";
-      }
+  //     alert("boolean");
+  //     return "boolean";
+  //     // Chaves englobando o valor
+  //   } else if (exp.includes("{") && exp.includes("}")) {
+  //     // Se tiver aspas simples é string
+  //     if (exp.includes("'")) {
+  //       alert("string");
+  //       return "string";
+  //     }
 
-      alert("boolean");
-      return "boolean";
-      // Chaves englobando o valor
-    } else if (exp.includes("{") && exp.includes("}")) {
-      // Se tiver aspas simples é string
-      if (exp.includes("'")) {
-        alert("string");
-        return "string";
-      }
+  //     if (exp.match(/[a-z]/i)) {
+  //       alert("error");
+  //       return "error";
+  //     } else alert("number");
+  //     return "number";
+  //   } else {
+  //     alert("error");
+  //     return null;
+  //   }
+  // };
 
-      if (exp.match(/[a-z]/i)) {
-        alert("error");
-        return "error";
-      } else alert("number");
-      return "number";
-    } else {
-      alert("error");
-      return null;
-    }
-  };
+  // const checkExpOperator = (exp) => {
+  //   const operators = ["/=", "/p", "/l", "/t", "/r", "/i"];
+  //   switch (exp) {
+  //     // /= é o operador de atribuição
+  //     case exp.includes("/="):
+  //       console.log("entrou");
+  //       // O que tem antes é o nome da variavel
+  //       // O que vier depois é o valor, temos que ver o tipo (string, boolean, number)
+  //       // Se for string, tem que ter aspas simples, boolean, true ou false, number, só numeros
 
-  const checkExpOperator = (exp) => {
-    const operators = ["/=", "/p", "/l", "/t", "/r", "/i"];
-    console.log(exp);
-    switch (exp) {
-      // /= é o operador de atribuição
-      case exp.includes("/="):
-        console.log("entrou");
-        // O que tem antes é o nome da variavel
-        // O que vier depois é o valor, temos que ver o tipo (string, boolean, number)
-        // Se for string, tem que ter aspas simples, boolean, true ou false, number, só numeros
+  //       // Separar a expressão em duas partes
+  //       let expArray = exp.split(operators[0]);
 
-        // Separar a expressão em duas partes
-        let expArray = exp.split(operators[0]);
+  //       // Verificar o tipo do valor resultado
+  //       let type = checkValueType(expArray[1]);
 
-        // Verificar o tipo do valor resultado
-        let type = checkValueType(expArray[1]);
+  //       console.log(type);
 
-        console.log(type);
+  //       break;
 
-        break;
+  //     // /p é o operador de print no terminal
+  //     case exp.includes(operators[1]):
+  //       break;
 
-      // /p é o operador de print no terminal
-      case exp.includes(operators[1]):
-        break;
+  //     // /l é o operador de definição de lista
+  //     case exp.includes(operators[2]):
+  //       break;
 
-      // /l é o operador de definição de lista
-      case exp.includes(operators[2]):
-        break;
+  //     // /t é o operador de definição de tabela
+  //     case exp.includes(operators[3]):
+  //       break;
 
-      // /t é o operador de definição de tabela
-      case exp.includes(operators[3]):
-        break;
+  //     // /r é o operador de repetição
+  //     case exp.includes(operators[4]):
+  //       break;
 
-      // /r é o operador de repetição
-      case exp.includes(operators[4]):
-        break;
+  //     // /i é o ternário
+  //     case exp.includes(operators[5]):
+  //       break;
 
-      // /i é o ternário
-      case exp.includes(operators[5]):
-        break;
+  //     default:
+  //       break;
+  //   }
+  // };
 
-      default:
-        break;
-    }
-  };
-
-  const checkExpression = (exp) => {
-    // checkError(exp);
-    checkExpOperator(exp);
-  };
 
   const handleAddItem = () => {
-    checkExpression(expression);
-
     if (!expression) {
       return toast({
         title: "Nenhum valor.",
@@ -130,27 +121,117 @@ export function AppProvider({ children }) {
       });
     } else {
       if (expression.includes("$")) {
-        // match way
-        // const variableName = expression.match(/\$[a-zA-Z0-9]+/g);
         const variableName = expression.split(" ")[0].replace("$", "");
 
-        const newItem = {
-          id: variableList.length,
-          expression: expression,
-          variable: variableName,
-          values: eval(expression.split(" ")[2]),
-          type: checkValueType(expression.split(" ")[2]),
-        };
-        setVariableList((prev) => [...prev, newItem]);
-      } else {
-        const expressionReplace = expression.replace(/[^-()\d/*+.]/g, '');
-        const result = eval(expressionReplace);
-        setResult(result);
+        if (expression.includes('/=')) {
+          // split expression
+          const expressionArray = expression.split('/=');
+
+          // get value and check type
+          const value = expressionArray[1];
+
+          // process value
+          if (value.includes('{') && value.includes('}')) { // se o usuario entrar com { }
+            const valueSemAsChaves = value.replace('{', '').replace('}', '') // tirou as chaves
+
+            let newItem = {}
+
+            // identificar se o que tem dentro das chaves é uma string ou um numero
+            if (valueSemAsChaves.includes("'")) {
+              // se tiver aspas simples, é string
+              const valueSemAspas = valueSemAsChaves.replace("'", "")
+                 // tirou as aspas
+              const valueType = "string";
+              // create new item
+              newItem = {
+                id: variableList.length,
+                expression: expression,
+                variable: variableName,
+                values: valueSemAspas,
+                type: valueType,
+              };
+            } else if (valueSemAsChaves.includes("true") || valueSemAsChaves.includes("false")) {
+              // se tiver true ou false, é boolean
+              const valueType = "boolean";
+              // create new item
+              newItem = {
+                id: variableList.length,
+                expression: expression,
+                variable: variableName,
+                values: valueSemAsChaves,
+                type: valueType,
+              };
+            } else if (valueSemAsChaves.match(/[0-9]/g)) {
+              // se tiver numeros, é number
+              const valueType = "number";
+              // create new item
+              newItem = {
+                id: variableList.length,
+                expression: expression,
+                variable: variableName,
+                values: valueSemAsChaves,
+                type: valueType,
+              };
+            }
+
+            // check if variable already exists
+            const variableExists = variableList.find((item) => item.variable === variableName);
+              
+            if (variableExists) {
+              // if variable exists, update it
+              const updatedList = variableList.map((item) => {
+                if (item.variable === variableName) {
+                  return newItem;
+                }
+                return item;
+              });
+
+              setVariableList(updatedList);
+            } else if (newItem) {
+              // if variable doesn't exist, create it
+              setVariableList([...variableList, newItem]);
+            }
+
+
+
+          } else if (value.includes("'")) {
+            // se tiver aspas simples, é string
+            const valueSemAspas = value.replace("'", "").replace("'", ""); // tirou as aspas
+            const valueType = "string";
+            // create new item
+            const newItem = {
+              id: variableList.length,
+              expression: expression,
+              variable: variableName,
+              values: valueSemAspas,
+              type: valueType,
+            };
+
+            // check if variable already exists
+            const variableExists = variableList.find((item) => item.variable === variableName);
+
+            if (variableExists) {
+              // if variable exists, update it
+              const updatedList = variableList.map((item) => {
+                if (item.variable === variableName) {
+                  return newItem;
+                }
+                return item;
+              });
+
+              setVariableList(updatedList);
+            } else {
+              // if variable doesn't exist, create it
+              setVariableList([...variableList, newItem]);
+            }
+          }
+        } else {
+          const expressionReplace = expression.replace(/[^-()\d/*+.]/g, '');
+          const result = eval(expressionReplace);
+          setResult(result);
+        }
       }
     }
-
-
-    // checkExpression(newItem);
 
     resetVariables();
   };
